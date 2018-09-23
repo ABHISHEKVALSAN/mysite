1b6f7305114358a2e2b15c3876fb934a1ebb7f67
Abhi <abhiavk@ubuntu.ubuntu-domain>
2018-09-23 16:58:06 +0530

    changed virtualEnv

--- mysiteEnv/bin/activate.fish
+++ mysiteEnv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/abhiavk/mysite/mysiteEnv"
+set -gx VIRTUAL_ENV "/home/abhiavk/git/mysite/mysiteEnv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
