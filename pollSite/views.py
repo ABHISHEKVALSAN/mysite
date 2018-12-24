39f301b63cef7c42abb978f8ee6ff91675ae0fa5
ABHISHEKVALSAN <abhishekvalsan9999@gmail.com>
2018-12-24 21:28:52 +0530

    First working site that gives 5 random sites to rate.

--- pollSite/views.py
+++ pollSite/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from .models import Choice, PageUrl
+
+import random
+"""
 class IndexView(generic.ListView):
     template_name = 'pollSite/index.html'
     context_object_name = 'question_list'
@@ -15,20 +18,44 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = PageUrl
     template_name = 'pollSite/results.html'
+"""
+num=0
+def index(request):
+	global	num
+	num			=	0
+	question_id	=	random.randint(1,6)
+	question	=	get_object_or_404(PageUrl, pk=question_id)
+	args		=	{'question':question}
+	return render(request,'pollSite/index.html',args)
+def detail(request,pk):
+	question	=	get_object_or_404(PageUrl, pk=pk)
+	args		=	{'object':question}
+	return render(request,'pollSite/detail.html',args)
+def results(request,pk):
+	question	=	get_object_or_404(PageUrl, pk=pk)
+	args		=	{'object':question}
+	return render(request,'pollSite/results.html',args)
+def thanks(request):
+	args		=	{}
+	return render(request,'pollSite/thanks.html',args)
 def vote(request, question_id):
-    question = get_object_or_404(PageUrl, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'pollSite/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('pollSite:results', args=(question.id,)))
+	global num
+	question = get_object_or_404(PageUrl, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		# Redisplay the question voting form.
+		return render(request, 'pollSite/detail.html', {
+		'question': question,
+			'error_message': "You didn't select a choice.",
+			})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		nextQuestion	=	random.choice(list(PageUrl.objects.order_by('id')))
+		if num==5:
+			num=0
+			args={}
+			return HttpResponseRedirect(reverse('pollSite:thanks'))
+		num+=1
+		return HttpResponseRedirect(reverse('pollSite:detail', args=(nextQuestion.id,)))
